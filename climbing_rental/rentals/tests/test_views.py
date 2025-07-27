22c2b234773806a4e0fb5957377791636e0c3a03
BiaexD <bartos.dabrowski@gmail.com>
2025-07-27 17:58:23 +0200

    Add describes

--- climbing_rental/rentals/tests/test_views.py
+++ climbing_rental/rentals/tests/test_views.py
@@ -18,11 +18,14 @@ Equipment.objects.create(...) - tworzymy sprzet, dzieki temu widok ma co wyswiet
 
 url - pobieramy sciezke /equipment/
 
-response - wysylamy symulowane zadanie GET
+response - wysylamy symulowane zadanie GET(get pokazuje nam cos)
 
 assert response 200 - sprawdzamy czy serwer zwrocil OK
 
 assert "Lina Beal" - sprawdzamy czy nazwa sprzetu pojawila sie w HTML
+
+response.content – to surowy HTML w bajtach (b'<html>...').
+.decode() zamienia go na zwykły tekst ('<html>...'), żebyśmy mogli go przeszukać.
 """
 
 @pytest.mark.django_db
@@ -114,7 +117,14 @@ def test_home_view(client):
     assert "Via ferrata" in content
 
 
+"""
+client.session - pobieramy dane z sesji testowego uzytkownika (cos na ksztalt ciasteczek) - Django przechowuje koszyk w tej sesji – w postaci słownika (dict).
+session['cart'] - dodajemy 2 sprzety do koszyka tak jakby klient umiescil je za pomoca dodaj
+session.save() - zapisujemy te zmiany w sesji
 
+url = reverse('cart') – funkcja Django, która tłumaczy nazwę ścieżki (cart) na jej prawdziwy adres URL
+client.get(url) - symulujemy wejscie na strone uzytkownika /cart/
+"""
 @pytest.mark.django_db
 def test_cart_view(client):
     category = Category.objects.create(name="Zima")
@@ -141,7 +151,33 @@ def test_cart_view(client):
     assert "200.00" in content
 
 
+"""
+args=[equipment.pk]:
+- oznacza, że podstawiamy ID naszego sprzętu do ścieżki URL.
 
+client.post(url):
+- To jest POST, czyli wysyłamy formularz – tak jakbyśmy kliknęli przycisk „Dodaj do koszyka”.
+- symuluje wysylanie formularza
+
+response.status_code == 302:
+- sprawdzamy czy nasz post zadzialal i wlozyl cos do koszyka
+
+session = client.session:
+- Otwieramy „plecak użytkownika” – czyli sesję.
+- Django przechowuje koszyk w tej sesji – w postaci słownika (dict).
+
+cart = session.get('cart', {}):
+- Pobieramy zawartość koszyka z sesji.
+- Jeśli koszyka nie ma (pierwsze użycie) – dostaniemy pusty słownik {}.
+                               
+assert str(equipment.pk) in cart:
+- Sprawdzamy, czy nasz sprzęt jest w koszyku.
+- equipment.pk to ID sprzętu, np. 3, ale koszyk przechowuje to jako tekst '3', więc musimy zrobić str(...)
+
+assert cart[str(equipment.pk)] == 1:
+- Sprawdzamy, czy ilość danego sprzętu w koszyku wynosi 1.
+- Bo raz kliknęliśmy „Dodaj do koszyka”, więc powinien być 1.
+"""
 @pytest.mark.django_db
 def test_add_to_cart_view(client):
     category = Category.objects.create(name="Skaly")
@@ -157,6 +193,7 @@ def test_add_to_cart_view(client):
     response = client.post(url)
     assert response.status_code == 302
 
-    cart = client.session['cart']
+    session = client.session
+    cart = session.get('cart', {})
     assert str(equipment.pk) in cart
     assert cart[str(equipment.pk)] == 1
